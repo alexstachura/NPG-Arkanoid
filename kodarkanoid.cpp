ef1acca4efabd9e9742fbc6c872d90add797690b
rylski123 <103134046+rylski123@users.noreply.github.com>
2022-06-08 14:32:08 +0200

    Update kodarkanoid.cpp

--- kodarkanoid.cpp
+++ kodarkanoid.cpp
@@ -112,3 +112,9 @@ void __fastcall TForm1::lewoTimer(TObject *Sender)
 {
     if (p->Left>10) p->Left-=10;
 }
+//---------------------------------------------------------------------------
+//timer_prawo
+void __fastcall TForm1::prawoTimer(TObject *Sender)
+{
+    if (p->Left+p->Width<tlo->Width-10) p->Left+=10;
+}
