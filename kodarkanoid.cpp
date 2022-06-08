9f5bad563106fda3b021de5b390768a741f362ee
rylski123 <103134046+rylski123@users.noreply.github.com>
2022-06-08 14:33:09 +0200

    Update kodarkanoid.cpp

--- kodarkanoid.cpp
+++ kodarkanoid.cpp
@@ -142,3 +142,10 @@ void __fastcall TForm1::FormCreate(TObject *Sender)
    MediaPlayer1->FileName = "snd/Arkanoid_music.mp3";
    MediaPlayer1->Open();
 }
+//---------------------------------------------------------------------------
+#wywolanie_dzwieku
+void __fastcall TForm1::FormClose(TObject *Sender, TCloseAction &Action)
+{
+   MediaPlayer1->Close();
+}
+//---------------------------------------------------------------------------
