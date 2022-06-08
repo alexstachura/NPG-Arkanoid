45d9d1c4eabb0a5e4ce3bad94795375fae9b8f4a
rylski123 <103134046+rylski123@users.noreply.github.com>
2022-06-08 14:12:48 +0200

    Update kodarkanoid.cpp

--- kodarkanoid.cpp
+++ kodarkanoid.cpp
@@ -12,3 +12,11 @@ TForm1 *Form1;
 #odliczanie_i_kolizja
 int x=-8, y=-8;
 int do_wygranej=12; 
+
+bool kolizja(TImage *pilka, TImage *cegla)
+{
+   if( pilka->Left >= cegla->Left-pilka->Width && pilka->Left <= cegla->Left+cegla->Width &&
+       pilka->Top >= cegla->Top-pilka->Height && pilka->Top <= cegla->Top+cegla->Height)
+      return true;
+      else return false;
+}
