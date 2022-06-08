1a2b9d62c46ecc0305333d5420dd3c9042ad55ec
rylski123 <103134046+rylski123@users.noreply.github.com>
2022-06-08 14:29:44 +0200

    Update kodarkanoid.cpp

--- kodarkanoid.cpp
+++ kodarkanoid.cpp
@@ -63,3 +63,30 @@ void __fastcall TForm1::Timer_pilkaTimer(TObject *Sender)
         Button1->Caption = "Wygrana! Jeszcze raz?";
         Button1->Visible=true;
      }
+
+    //blok1
+    if (kolizja(b,Image1) && Image1->Visible==true) {x=-x; y=-y; do_wygranej--; Image1->Visible=false;}
+    //blok2
+    if (kolizja(b,Image2) && Image2->Visible==true) {x=-x; y=-y; do_wygranej--; Image2->Visible=false;}
+    //blok3
+    if (kolizja(b,Image3) && Image3->Visible==true) {x=-x; y=-y; do_wygranej--; Image3->Visible=false;}
+    //blok4
+    if (kolizja(b,Image4) && Image4->Visible==true) {x=-x; y=-y; do_wygranej--; Image4->Visible=false;}
+    //blok5
+    if (kolizja(b,Image5) && Image5->Visible==true) {x=-x; y=-y; do_wygranej--; Image5->Visible=false;}
+    //blok6
+    if (kolizja(b,Image6) && Image6->Visible==true) {x=-x; y=-y; do_wygranej--; Image6->Visible=false;}
+    //blok7
+    if (kolizja(b,Image7) && Image7->Visible==true) {x=-x; y=-y; do_wygranej--; Image7->Visible=false;}
+    //blok8
+    if (kolizja(b,Image8) && Image8->Visible==true) {x=-x; y=-y; do_wygranej--; Image8->Visible=false;}
+    //blok9
+    if (kolizja(b,Image9) && Image9->Visible==true) {x=-x; y=-y; do_wygranej--; Image9->Visible=false;}
+    //blok10
+    if (kolizja(b,Image10)&& Image10->Visible==true) {x=-x; y=-y; do_wygranej--; Image10->Visible=false;}
+    //blok11
+    if (kolizja(b,Image11) && Image11->Visible==true) {x=-x; y=-y; do_wygranej--; Image11->Visible=false;}
+    //blok12
+    if (kolizja(b,Image12) && Image12->Visible==true) {x=-x; y=-y; do_wygranej--; Image12->Visible=false;}
+
+}
